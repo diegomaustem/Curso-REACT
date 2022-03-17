87b55b168418d564e9ba3afb9d141ca10354516e
Diego Maustem <diego.maustem@gmail.com>
2022-03-17 17:33:42 -0300

    trabalhando-com-states

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
 import React, { Component } from "react";
 
-// Aplicando o Class Component ::: 
-class Equipe extends Component{
+
+class App extends Component{
+
+    constructor(props){
+
+        super(props);
+        this.state = {
+            nome: 'CONTADOR DE HIGS',
+            contador: 0
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar(){
+        
+        let state = this.state;
+        state.contador += 1;
+        this.setState(state);
+    }
+
+    diminuir(){
+
+        let state = this.state;
+        state.contador -= 1;
+        this.setState(state);
+    }
 
     render(){
         return(
             <div>
-                <Informacao nome={this.props.nome} idade={this.props.idade} cargo={this.props.cargo}/>
+                <h2>
+                   {this.state.nome}
+                </h2> 
+                <h2>
+                   <button onClick={this.diminuir}>-</button>
+                        {this.state.contador}
+                   <button onClick={this.aumentar}>+</button>
+                </h2>
             </div>
         );
     }
 
 }
 
-class Informacao extends Component{
-
-    render(){
-        return(
-            <div>
-                <h2> Profissional: {this.props.nome} </h2>
-                <h2> Idade: {this.props.idade} </h2>
-                <h2> Cargo: {this.props.cargo} </h2>
-            </div>
-        );
-    }
-
-}
-
-function App(){
-
-     return(
-        <div>
-            <h1>Dados Pessoais</h1>
-            <Equipe nome=" Diego" cargo="Programador" idade="28"/>
-        </div>
-     );
-}
 
 export default App;
