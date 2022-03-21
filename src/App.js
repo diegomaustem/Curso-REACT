d149c55439e109d62957ee932368ffd9d66bd9c3
Diego Maustem <diego.maustem@gmail.com>
2022-03-21 11:41:49 -0300

    Trablhando-com-listas-aula-41

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
-import Membro from './components/Membro/membro';
-
+import Feed from './components/Feed';
 
 class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            feed:
+            [
+                {id: 1, username: 'Diego', curtidas: 10, comentarios: 2}, 
+                {id: 2, username: 'Marcio', curtidas: 120, comentarios: 15},
+                {id: 3, username: 'Jabatão', curtidas: 851, comentarios: 68},
+                {id: 4, username: 'Clayton', curtidas: 22, comentarios: 18},
+                {id: 5, username: 'Ricardo', curtidas: 1, comentarios: 1}
+            ]
+        }
+
+    }
 
     render(){
         return(
             <div>
-                <h2>
-                   <Membro nome="Vistante" idade="28"/>
-                </h2> 
+                {this.state.feed.map((item)=>{
+                    return(
+                       <Feed 
+                            key={item.id} 
+                            username={item.username} 
+                            curtidas={item.curtidas} 
+                            comentarios={item.comentarios}
+                       />
+                    );
+                })
+
+                }
             </div>
         );
     }
