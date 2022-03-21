23ae63aecea28d7382d4a0ea21aae35828159ca0
Diego Maustem <diego.maustem@gmail.com>
2022-03-21 10:59:33 -0300

    aula-40-renderização-condicional

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
 import React, { Component } from "react";
+import Membro from './components/Membro/membro';
 
 
 class App extends Component{
-
-    constructor(props){
-
-        super(props);
-        this.state = {
-            nome: 'CONTADOR DE HIGS',
-            contador: 0
-        };
-
-        this.aumentar = this.aumentar.bind(this);
-        this.diminuir = this.diminuir.bind(this);
-    }
-
-    aumentar(){
-        
-        let state = this.state;
-        state.contador += 1;
-        this.setState(state);
-    }
-
-    diminuir(){
-
-        let state = this.state;
-        state.contador -= 1;
-        this.setState(state);
-    }
 
     render(){
         return(
             <div>
                 <h2>
-                   {this.state.nome}
+                   <Membro nome="Vistante" idade="28"/>
                 </h2> 
-                <h2>
-                   <button onClick={this.diminuir}>-</button>
-                        {this.state.contador}
-                   <button onClick={this.aumentar}>+</button>
-                </h2>
             </div>
         );
     }
---
+++ src/components/Membro/membro.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Membro extends Component{
+
+    constructor(props){
+
+        super(props);
+        this.state = {
+            status: false,
+        };
+
+        this.sair = this.sair.bind(this);
+        this.entrar = this.entrar.bind(this);
+    }
+
+    entrar() {
+        this.setState({status: true});
+    }
+
+    sair(){
+        this.setState({status: false});
+    }
+
+    render(){
+
+        return(
+            <div>
+                {this.state.status ?
+                    <div>
+                        <h2>Bem vindo ao sistema!</h2>
+                        <button onClick={this.sair}>Entrar no sistema</button>
+                    </div> :
+
+                    <div>
+                         <h2>Olá visitante, faça o login!</h2>
+                         <button onClick={this.entrar}>Entrar no sistema</button>
+                    </div>
+                }
+            </div>            
+        );
+    }
+}
+
+export default Membro;
